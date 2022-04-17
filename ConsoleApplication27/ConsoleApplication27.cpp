6cac5babf2ed997aa527cd29f7eeada95b495ab4
edgre <egorprudnikov71@gmail.com>
2022-04-17 08:44:22 +0700

    Начало построение дерева кодирования

--- ConsoleApplication27/ConsoleApplication27.cpp
+++ ConsoleApplication27/ConsoleApplication27.cpp
@@ -3,47 +3,97 @@
 using namespace std;
 
 struct Node;
+struct Node
+{   Node* r;
+    Node* l;
+    Node* next;
+    int size;
+    char* key;
+    Node(int n = 0, char key = NULL);
+};
 
 struct tree
 {
-    Node* a;
-    tree* next;
+    Node* first;
+    Node* last;
+    tree();
 };
 
-struct Node
-{
-    tree* l;
-    tree* r;
-    int size;
-    char key;
-};
+tree::tree() { first = NULL; last = NULL; }
 
-void Add(tree*& head, int n, char key)
+Node::Node(int n, char k)
 {
-    tree* tmp, * p;
-    tmp = head;
-    while (tmp->next != NULL)
-        tmp = tmp->next;
-    p = new tree;
-    tmp->next = p;
-    tmp->a->key = key;
-    tmp->a->size = n;
-};
-
-void swap(tree* m1, tree* m2, tree*& head)
-
-{
-    m2->next = m1;
-    m1->next = m2->next;
+    size = n; key = (char*)k;
+    r = NULL; l = NULL; 
+    next = NULL;
 }
 
+void Add(tree*b, int n, char key)
+{
+    Node* p = new Node(n, key);
+    if (b->first == NULL)
+    {
+        b->first = p;
+        b->last = p;
+        return;
+       
+    }
+    b->last->next = p;
+    b->last = p;
+};
+
+void out(tree* b)
+{
+    int u = 0;
+    Node* tmp;
+    tmp = b->first;
+    while (tmp)
+    {
+        cout << tmp->size << ' ';
+        tmp = tmp->next;
+    }
+}
+
+void swap(Node* a, Node* b, tree* c)
+{
+    Node* pred = c->first;
+
+    if (a!=c->first) {
+        while (pred->next != a)
+            pred = pred->next;
+         pred->next = b;
+    }
+    if (a == c->first) c->first = b;
+    if (b != c->last) {
+        a->next = b->next; 
+    }
+    if (b == c->last) 
+    {
+        c->last = a; a->next = NULL;
+    }
+    b->next = a;
+    
+   
+}
+
+void newnode(tree* b)
+{
+    Node* p = new Node;
+    Node* beg = b->first;
+    p->size = beg->size + beg->next->size;
+    p->key = strcat(beg->key, beg->next->key);
+    p->l = beg;
+    p->r = beg->next;
+    p->next = beg->next->next;
+    b->first = p;
+}
 
 int main()
 {
     int asc[256];
     int n = 0;
     for (int i = 0; i < 256; i++) asc[i] = 0;
-    fstream fc("c.txt");
+    fstream fc("текст.txt");
     char sim;
     fc >> sim;
     while (fc)
@@ -51,35 +101,39 @@ int main()
         asc[(int)sim]++;
         fc >> sim;
     };
-    tree* b = NULL;
+    tree* b = new tree;
     for (int i = 0; i < 256; i++)
     {
         cout << asc[i] << ' ';
     }
-
-    for (int i = 0; i < 256; i++)
-    {
-        if (asc[i] != 0)
+    cout << endl;
+     n = 0;
+     for (int i = 0; i < 256; i++)
+     {
+         if (asc[i] != 0)
+         {
+             Add(b, asc[i], (char)i);
+             n++;
+         }
+     }
+     cout << n << endl;
+     out(b);
+     cout << endl;
+for (int i = 0; i <= n - 2; i++)
+{
+    Node* tmp = b->first;
+    for (int j = 0; j <= n - 2; j++)
+    { 
+        
+        if (tmp->size > tmp->next->size)
         {
-            Add(b, asc[i], (char)i);
+            
+            swap(tmp, tmp->next, b);
         }
 
-    }
+       }
 
-    while (b->next != NULL)
-    {
-        tree*& head = b;
-        while (head->next != NULL)
-        {
-            tree*& headl = b;
-            while (head->next != NULL)
-            {
-                if (headl > headl->next) swap(headl, headl->next, b);
-                headl = headl->next;
-            }
-            head = head->next;
-        }
-
-    }
-
+   }
+out(b); cout << endl;
+newnode(b); out(b);
 }
